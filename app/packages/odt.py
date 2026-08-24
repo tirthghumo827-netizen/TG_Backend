f2e74e64f45b9267f59b3e36493a89f3cda7f4dc
theks2502 <theks2502@gmail.com>
2026-08-24 20:41:58 +0530

    Ujjain Page

--- app/packages/odt.py
+++ app/packages/odt.py
@@ -443,7 +443,7 @@ ODT_WHATSAPP_GROUPS = {
     # "2026-07-26": "https://chat.whatsapp.com/JkflPYXwYqzIfVEe8rmMUf?s=cl&p=i&mlu=0&ilr=0",  # B10
     # "2026-08-22": "https://chat.whatsapp.com/HIwU7EwT5iyAkQhX73ZP81?s=cl&p=i&mlu=0&ilr=0" , # Halali Trek
     "2026-09-05" : "https://chat.whatsapp.com/G9cEuK3Vb9b4KtizF2TbVu?s=sw&p=a&ilr=4", # Halali Batch 2 
-    "2026-09-12" : "https://chat.whatsapp.com/FyqDe4aK99TGxhhgbd9pMX?s=sw&p=a&ilr=4", # Ujjain Batch 1 
+    "2026-09-19" : "https://chat.whatsapp.com/FyqDe4aK99TGxhhgbd9pMX?s=sw&p=a&ilr=4", # Ujjain Batch 1 
     "2026-09-26" : "https://chat.whatsapp.com/LUSThbpdQ9D5kdnchJ5gEv?s=sw&p=a&ilr=4", # Ujjain Batch 2
     # add more trek dates here as needed
 }
